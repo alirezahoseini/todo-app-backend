ef92fe1c0fff54ea51c06fef4791922eb498cc35
Alirea Hosseini <mehdimj0161@gmail.com>
2024-07-23 13:07:52 +0330

    add the actual mongo database to the db file

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,7 @@ const connectToDB = async () => {
             console.log('DB IS OK ---> DBCONECTION');
             return false
         } else {
-            mongoose.connect('mongodb://localhost:27017/test');
+            mongoose.connect('mongodb+srv://mehdimj0161:NargesHasani@cluster0.qbuo1hj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
             console.log('Db Connected :))');
         }
     } catch (error) {
